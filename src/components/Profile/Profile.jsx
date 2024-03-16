ffc4bbb304f63f8d06a20f96be25c2e1e125c2aa
Artem <atremat@gmail.com>
2024-03-16 14:26:14 +0200

    task1 css finished

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import styles from "./Profile.module.css";
+
 const Profile = ({
   name,
   tag,
@@ -6,26 +9,32 @@ const Profile = ({
   stats: { followers, views, likes },
 }) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={clsx(styles["profile-container"])}>
+      <div className={clsx(styles["profile-info-wrapper"])}>
+        <img
+          src={image}
+          alt="User avatar"
+          className={clsx(styles["profile-img"])}
+        />
+        <p className={clsx(styles["profile-name"])}>{name}</p>
+        <p className={clsx(styles["profile-tag"])}>@{tag}</p>
+        <p className={clsx(styles["profile-location"])}>{location}</p>
       </div>
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{followers}</span>
+      <ul className={clsx(styles["profile-list-stat"])}>
+        <li className={clsx(styles["profile-list-item"])}>
+          <span className={clsx(styles["profile-list-header"])}>Followers</span>
+          <span className={clsx(styles["profile-list-quantity"])}>
+            {followers}
+          </span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{views}</span>
+        <li className={clsx(styles["profile-list-item"])}>
+          <span className={clsx(styles["profile-list-header"])}>Views</span>
+          <span className={clsx(styles["profile-list-quantity"])}>{views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{likes}</span>
+        <li className={clsx(styles["profile-list-item"])}>
+          <span className={clsx(styles["profile-list-header"])}>Likes</span>
+          <span className={clsx(styles["profile-list-quantity"])}>{likes}</span>
         </li>
       </ul>
     </div>
